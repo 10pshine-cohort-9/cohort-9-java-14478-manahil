f11271e94fc9820e797c3387aa13769795e23990
Manahil Waheed <2380240@szabist-isb.pk>
2026-08-21 16:46:48 +0500

    Use isolated database for repository tests

--- src/test/java/com/example/cohort_9_java_14478_manahil/repository/ContactRepositoryTest.java
+++ src/test/java/com/example/cohort_9_java_14478_manahil/repository/ContactRepositoryTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ContactRepositoryTest {
 
     @Autowired
